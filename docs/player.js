f30d1843eea2190338e7fce7fc51035f71da9463
3haruka14 <sayu_gekka@yahoo.co.jp>
2017-05-11 12:31:52 +0900

    順番やつに勝つ関数を別にまとめました

--- docs/player.js
+++ docs/player.js
@@ -3,13 +3,17 @@ const CHOKI = 1;
 const PA = 2;
 
 let matches = 0;
-function action(oppornent){
+
+function actionAgainstOmoteUra2(){
   matches = matches + 1;
   const remainder = matches % 3;
   const isEven = remainder == 0;
-  console.log(`${matches} % 3 = ${remainder} : ${isEven}`);
   if(remainder==0){
-    return CHOKI
+    return CHOKI;
   }
   return GU;
 }
+
+function action(oppornent)
+{ let result =actionAgainstOmoteUra2();
+return result;}
