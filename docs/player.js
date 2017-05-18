9c15bc458e45dc297549cf3c73f47282c1ddfa75
3haruka14 <sayu_gekka@yahoo.co.jp>
2017-05-18 11:43:12 +0900

    第五講義
    だけどlecture4に更新してるこの（）

--- docs/player.js
+++ docs/player.js
@@ -4,16 +4,43 @@ const PA = 2;
 
 let matches = 0;
 
+function countup(){
+  matches=matches+1;}
+
+function actionAgainstCHOKI(){
+  return GU;
+}
+
+function actionAgainstOmoteUra(){
+  const remainder = matches % 2;
+  if(remainder==0){
+    return GU;
+  }
+  return PA;
+}
+
 function actionAgainstOmoteUra2(){
-  matches = matches + 1;
   const remainder = matches % 3;
-  const isEven = remainder == 0;
   if(remainder==0){
     return CHOKI;
   }
   return GU;
 }
+//なんで動かないんだろうね？
+function kirei(a){
+  if(a==50){a=0;}
+}
 
-function action(oppornent)
-{ let result =actionAgainstOmoteUra2();
+//"fighter::choki-lover", "fighter::odd-even", "fighter::on-third", "fighter::rotation", "fighter::fizzbuzz", "fighter::random"
+function action(oppornent){
+  let result = GU;
+  countup();
+   if(oppornent=="fighter::choki-lover"){
+   result = actionAgainstCHOKI();}
+  else if(oppornent=="fighter::odd-even")
+   {result = actionAgainstOmoteUra();}
+  else if(oppornent=="fighter::on-third")
+  {result = actionAgainstOmoteUra2();}
+  kirei(matches);
+console.log(matches);
 return result;}
